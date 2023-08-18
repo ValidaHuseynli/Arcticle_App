6d8bea8bcba5bce8fea033c5122842115663615a
ValidaHuseynli <135136504+ValidaHuseynli@users.noreply.github.com>
2023-08-19 00:56:09 +0400

    Update Article.java

--- src/main/java/com/articleapp/model/Article.java
+++ src/main/java/com/articleapp/model/Article.java
@@ -1,10 +1,30 @@
 package com.articleapp.model;
 
+import java.time.LocalDate;
+
 public class Article {
     private int id;
     private String title;
     private String article;
     private String username;
+    private LocalDate created_at;
+    private LocalDate updated_at;
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDate getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDate updated_at) {
+        this.updated_at = updated_at;
+    }
 
     public int getId() {
         return id;
